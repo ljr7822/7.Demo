c97126d7e453563ea733214d9e3ae18f648ebace
iwenLjr <1816807688@qq.com>
2020-09-13 17:12:32 +0800

    完成了计算页面的跳转 完成不同应用间的跳转、参数的回调等

--- app/src/main/java/com/example/a7demo/MainActivity.kt
+++ app/src/main/java/com/example/a7demo/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.a7demo
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_detail.*
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,6 +14,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /**
+         * 计算页面按钮
+         */
         mbutton1.setOnClickListener {
             // 跳转到下一个页面并且计算
             // 创建Intent
@@ -27,17 +32,38 @@ class MainActivity : AppCompatActivity() {
                 startActivityForResult(this, 123)
             }
         }
+
+        /**
+         * 分享按钮
+         */
+        mbutton2.setOnClickListener {
+            // 使用隐式跳转到某个页面
+            Intent().apply {
+                action = "ljr.fenxian.wechar"
+                data = Uri.parse("content: 今天完成了Activity的跳转 很棒")
+            }.also {
+                // startActivity(it)
+                startActivityForResult(it,456)
+            }
+        }
     }
 
     /**
-     * 接收回调
+     * 接收计算回调
      */
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 123){
+            // 计算页面
             data?.getIntExtra("result",0).also {
                 // 将结果赋值到textview上
                 mResult.text = it.toString()
+            }
+        }else if (requestCode == 456){
+            // 分享页面
+            data?.getStringExtra("shareResult").also {
+                // 弹出提升框
+                Toast.makeText(this,it,Toast.LENGTH_LONG).show()
             }
         }
     }
